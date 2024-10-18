1597e4ab5dce55f9ad7dec027eac04d4df364062
Lisa Buche <60778585+Buchel9844@users.noreply.github.com>
2024-10-18 13:03:42 +1100

    update to have HOIs

--- code/6.1.Mean.abudance.sp.R
+++ code/6.1.Mean.abudance.sp.R
@@ -17,77 +17,107 @@ library(png)
 library(grid)
 
 # define you directory
-home.dic <- "/Users/lisabuche/Documents/Projects/Complexity_caracoles/"
+home.dic <- "/home/lbuche/Eco_Bayesian/Complexity_caracoles/"
 
+df_param_all <- read.csv(paste0(home.dic,
+                                "results/Chapt1_Parameters_values.csv"))
 
 ##
 abund.mean.df <- NULL
 Trophic.mean.df <- NULL
-for( focal in c("CETE","LEMA","HOMA","CHFU")){ # "CHFU","HOMA","CETE"
-  for( year in c("2019",'2020','2021')){
+for( focal.n in c("CETE","LEMA","HOMA","CHFU")){ # "CHFU","HOMA","CETE"
+  for( year.n in c("2019",'2020','2021')){
     for( complexity.level in c(1:3)){
+      print(paste0(focal.n,year.n,complexity.level))
       
-      complexity.animal <- c("group","family","species")[complexity.level]
-      complexity.plant <-c("class","family","code.plant")[complexity.level]
-      load(paste0(home.dic,"results/stan/Inclusion",
-                  focal,"_", year,"_",complexity.plant,"_",
-                  complexity.animal,".RData")
-           )
-
-      specific.int.names <- c(colnames(Inclusion_all$Inclusion_FV)[Inclusion_all$Inclusion_FV==1],
-              colnames(Inclusion_all$Inclusion_H)[Inclusion_all$Inclusion_H==1],
-                    colnames(Inclusion_all$Inclusion_ij)[Inclusion_all$Inclusion_ij==1])
+      complexity.animal.n <- c("group","family","species")[complexity.level]
+      complexity.plant.n <-c("class","family","code.plant")[complexity.level]
+      load(paste0(home.dic,"results/Inclusion",
+                  focal.n,"_", year.n,"_",complexity.plant.n,"_",
+                  complexity.animal.n,".RData")
+      )
       
-      abund.mean.df.i <- data.frame(focal = focal, year= year, 
-                                    complexity.animal = complexity.animal,
-                                    complexity.plant =  complexity.plant,
+      df_param_name_hat <- df_param_all %>%
+        dplyr::filter(.[,"focal"] == as.character(focal.n)  & 
+                        .[,"year"] == as.character(year.n)& 
+                        .[,"complexity.plant"] == as.character(complexity.plant.n) &
+                        !is.na(estimate_hat )) %>%
+        select(parameter,parameter_hat,complexity.plant) %>%
+        unique()
+      
+      specific.int.names <-  df_param_name_hat$parameter_hat
+      
+      
+      abund.mean.df.i <- data.frame(focal = focal.n, year= year.n, 
+                                    complexity.animal = complexity.animal.n,
+                                    complexity.plant =  complexity.plant.n,
                                     parameter_hat = c(Inclusion_all$SpNames,
-                                                 colnames(Inclusion_all$Inclusion_H),
-                                                 colnames(Inclusion_all$Inclusion_FV)),
+                                                      colnames(Inclusion_all$Inclusion_H),
+                                                      colnames(Inclusion_all$Inclusion_FV)),
                                     abund.sp = c(Inclusion_all$SpMatrix %>% colMeans(),
-                                                   Inclusion_all$SpMatrix_H %>% colMeans(),
-                                                   Inclusion_all$SpMatrix_FV %>% colMeans()),
+                                                 Inclusion_all$SpMatrix_H %>% colMeans(),
+                                                 Inclusion_all$SpMatrix_FV %>% colMeans()),
                                     abund.sp.min = c(Inclusion_all$SpMatrix %>% apply(.,2,min),
-                                                 Inclusion_all$SpMatrix_H %>% apply(.,2,min),
-                                                 Inclusion_all$SpMatrix_FV %>%  apply(.,2,min)),
+                                                     Inclusion_all$SpMatrix_H %>% apply(.,2,min),
+                                                     Inclusion_all$SpMatrix_FV %>%  apply(.,2,min)),
                                     abund.sp.max = c(Inclusion_all$SpMatrix %>%  apply(.,2,max),
-                                                 Inclusion_all$SpMatrix_H %>%  apply(.,2,max),
-                                                 Inclusion_all$SpMatrix_FV %>%  apply(.,2,max)),
+                                                     Inclusion_all$SpMatrix_H %>%  apply(.,2,max),
+                                                     Inclusion_all$SpMatrix_FV %>%  apply(.,2,max)),
                                     parameter = c(rep("Plant - plant",length(Inclusion_all$SpNames)-1),
-                                                      "Intraspecific",
-                                                      rep("Plant - herbivore",length(colnames(Inclusion_all$Inclusion_H))),
-                                                      rep("Plant - floral visitor",length(colnames(Inclusion_all$Inclusion_FV))))
-                                      ) %>%
+                                                  "Intraspecific",
+                                                  rep("Plant - herbivore",length(colnames(Inclusion_all$Inclusion_H))),
+                                                  rep("Plant - floral visitor",length(colnames(Inclusion_all$Inclusion_FV))))
+      ) %>%
         mutate(inclus = case_when( parameter_hat %in% specific.int.names ~ "inclus",
                                    T ~ "non-inclus"))
-               
       
-      Trophic.mean.df.i <- data.frame(focal = focal, year= year, 
-                                    complexity.animal = complexity.animal,
-                                    complexity.plant =  complexity.plant,
-                                    abund.sp.mean = c(mean(rowSums(Inclusion_all$SpMatrix[,!Inclusion_all$Intra])),
-                                                 Inclusion_all$SpMatrix[,Inclusion_all$Intra] %>% 
-                                                   mean(),
-                                                 mean(rowSums(Inclusion_all$SpMatrix_H)),
-                                                 mean(rowSums(Inclusion_all$SpMatrix_FV))),
-                                    abund.sp.median = c(median(rowSums(Inclusion_all$SpMatrix[,!Inclusion_all$Intra])),
-                                                 Inclusion_all$SpMatrix[,Inclusion_all$Intra] %>% 
-                                                   median(),
-                                                 median(rowSums(Inclusion_all$SpMatrix_H)),
-                                                 median(rowSums(Inclusion_all$SpMatrix_FV))),
-                                    abund.sp.max= c(max(Inclusion_all$SpMatrix[,!Inclusion_all$Intra]),
-                                                        max(Inclusion_all$SpMatrix[,Inclusion_all$Intra]),
-                                                        max(Inclusion_all$SpMatrix_H),
-                                                        max(Inclusion_all$SpMatrix_FV)),
-                                    abund.sp.min= c(min(Inclusion_all$SpMatrix[,!Inclusion_all$Intra]),
-                                                    min(Inclusion_all$SpMatrix[,Inclusion_all$Intra]),
-                                                    min(Inclusion_all$SpMatrix_H),
-                                                    min(Inclusion_all$SpMatrix_FV)),
-                                    parameter = c("Plant - plant",
+      if(sum(!specific.int.names %in% levels(as.factor(abund.mean.df.i$parameter_hat)))>0){
+        missing_HOIs <- specific.int.names[!specific.int.names %in% levels(as.factor(abund.mean.df.i$parameter_hat))]
+        param_hois <- df_param_name_hat$parameter[which(df_param_name_hat$parameter_hat==missing_HOIs)]
+        missing_HOIs1 <- strsplit( missing_HOIs, '_')[[1]][1]
+        missing_HOIs2 <- strsplit( missing_HOIs, '_')[[1]][2]
+        
+        abund.mean.df.i.hois <- data.frame(focal = focal.n, year= year.n, 
+                                           complexity.animal = complexity.animal.n,
+                                           complexity.plant =  complexity.plant.n,
+                                           parameter_hat = missing_HOIs,
+                                           inclus="inclus",
+                                           parameter = param_hois,
+                                           abund.sp = abund.mean.df.i$abund.sp[which(abund.mean.df.i$parameter_hat ==missing_HOIs1)]*
+                                             abund.mean.df.i$abund.sp[which(abund.mean.df.i$parameter_hat ==missing_HOIs2)],
+                                           abund.sp.max = abund.mean.df.i$abund.sp.max[which(abund.mean.df.i$parameter_hat ==missing_HOIs1)]*
+                                             abund.mean.df.i$abund.sp.max[which( abund.mean.df.i$parameter_hat ==missing_HOIs2)],
+                                           abund.sp.min = abund.mean.df.i$abund.sp.min[which(abund.mean.df.i$parameter_hat ==missing_HOIs1)]*
+                                             abund.mean.df.i$abund.sp.min[which(abund.mean.df.i$parameter_hat ==missing_HOIs2)])
+        abund.mean.df.i <- bind_rows( abund.mean.df.i, abund.mean.df.i.hois)
+      }
+      
+      Trophic.mean.df.i <- data.frame(focal = focal.n, year= year.n, 
+                                      complexity.animal = complexity.animal.n,
+                                      complexity.plant =  complexity.plant.n,
+                                      abund.sp.mean = c(mean(rowSums(Inclusion_all$SpMatrix[,!Inclusion_all$Intra])),
+                                                        Inclusion_all$SpMatrix[,Inclusion_all$Intra] %>% 
+                                                          mean(),
+                                                        mean(rowSums(Inclusion_all$SpMatrix_H)),
+                                                        mean(rowSums(Inclusion_all$SpMatrix_FV))),
+                                      abund.sp.median = c(median(rowSums(Inclusion_all$SpMatrix[,!Inclusion_all$Intra])),
+                                                          Inclusion_all$SpMatrix[,Inclusion_all$Intra] %>% 
+                                                            median(),
+                                                          median(rowSums(Inclusion_all$SpMatrix_H)),
+                                                          median(rowSums(Inclusion_all$SpMatrix_FV))),
+                                      abund.sp.max= c(max(Inclusion_all$SpMatrix[,!Inclusion_all$Intra]),
+                                                      max(Inclusion_all$SpMatrix[,Inclusion_all$Intra]),
+                                                      max(Inclusion_all$SpMatrix_H),
+                                                      max(Inclusion_all$SpMatrix_FV)),
+                                      abund.sp.min= c(min(Inclusion_all$SpMatrix[,!Inclusion_all$Intra]),
+                                                      min(Inclusion_all$SpMatrix[,Inclusion_all$Intra]),
+                                                      min(Inclusion_all$SpMatrix_H),
+                                                      min(Inclusion_all$SpMatrix_FV)),
+                                      parameter = c("Plant - plant",
                                                     "Intraspecific",
                                                     "Plant - herbivore",
                                                     "Plant - floral visitor")
-                                    )
+      )
       
       abund.mean.df <- bind_rows(   abund.mean.df,   abund.mean.df.i)
       Trophic.mean.df <- bind_rows( Trophic.mean.df,   Trophic.mean.df.i)
@@ -103,25 +133,6 @@ view(abund.mean.df)
 write.csv(abund.mean.df,
           "results/abund.mean.df.csv")
 
-# unstandard abundance
-plant.class <- read.csv(paste0(home.dic,"data/plant_code.csv"), sep=",")
-complexitylevel.plant <- levels(as.factor(plant.class[,complexity.plant]))
 
-SpData <- read.csv(paste0(home.dic,"data/SpData.csv"))
-SpDataFocal <- NULL
-for( n in c("CHFU","LEMA","CETE","HOMA")){
-SpDataFocal.n <- SpData[which(SpData$focal ==n),] %>%
-    mutate(abundance.all = (rowSums(.[c("Forb","Grass")]) - rowSums(.[c(n)]))) %>%
-    aggregate(abundance.all ~ focal + year, median)
-SpDataFocal <- bind_rows(SpDataFocal,SpDataFocal.n)
-       
-}  
-
-SpDataFocal <- SpDataFocal %>%
-  filter(year %in% c("2019","2020","2021"))
-str(SpDataFocal)
-max(SpDataFocal$abundance.all)
-
-median(SpDataFocal$abundance.all)
 
 
