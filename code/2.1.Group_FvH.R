a876f4c14cbdd28befd1bbf583c55e0a6b32a7a1
Lisa Buche <60778585+Buchel9844@users.noreply.github.com>
2024-10-18 13:03:15 +1100

    change to have only group with mean < var

--- code/2.1.Group_FvH.R
+++ code/2.1.Group_FvH.R
@@ -3,11 +3,11 @@
 #################################################################################
 #---- floral visitor----
 FVtoKeep_species <- groupinglevels$grouping[which(groupinglevels$HTL =="floral.visitor" &
-                                                    groupinglevels$complexity == "species" & groupinglevels$YesNo == "yes")]
+                                                    groupinglevels$complexity == "species" & (groupinglevels$YesNo == "yes"|groupinglevels$YesNo == "maybe"))]
 FVtoKeep_family <- groupinglevels$grouping[which(groupinglevels$HTL =="floral.visitor" &
-                                                   groupinglevels$complexity == "family" & groupinglevels$YesNo == "yes")]
+                                                   groupinglevels$complexity == "family" & (groupinglevels$YesNo == "yes"|groupinglevels$YesNo == "maybe"))]
 FVtoKeep_group <- groupinglevels$grouping[which(groupinglevels$HTL =="floral.visitor" &
-                                                  groupinglevels$complexity == "functional.groups" & groupinglevels$YesNo == "yes")]
+                                                  groupinglevels$complexity == "functional.groups" & (groupinglevels$YesNo == "yes"|groupinglevels$YesNo == "maybe"))]
 
 
 names(floral_visitor)[which(names(floral_visitor)=="plant")] <- "code.plant"
@@ -17,6 +17,14 @@ floral_visitor_species <- dplyr::select(floral_visitor,c("day","month","year","p
 floral_visitor_species <-stats::aggregate(number_visits ~ id_final +year +plot +subplot + code.plant,
                                           data= floral_visitor_species,
                                           sum)
+
+test.fv.sp <- floral_visitor_species %>%
+  group_by(id_final) %>%
+  summarise(mean.sp = mean(number_visits),
+            var.sp = var(number_visits)) %>%
+  filter(mean.sp<=var.sp) 
+FVtoKeep_species <- test.fv.sp$id_final[test.fv.sp$id_final %in% FVtoKeep_species]
+
 floral_visitor_species <-  dplyr::filter(floral_visitor_species,
                                          id_final %in% FVtoKeep_species)
 
@@ -32,6 +40,12 @@ floral_visitor_family <- dplyr::select(floral_visitor,c("plot","subplot","year",
 floral_visitor_family <-stats::aggregate(number_visits ~ family + year + plot + code.plant +subplot ,
                                          data=  floral_visitor_family,
                                          sum)
+test.fv.fam <- floral_visitor_family %>%
+  group_by(family) %>%
+  summarise(mean.sp = mean(number_visits),
+            var.sp = var(number_visits)) %>%
+  filter(mean.sp<=var.sp) 
+FVtoKeep_family <- test.fv.fam$family[test.fv.fam$family %in% FVtoKeep_family]
 
 floral_visitor_family  <- dplyr::filter(floral_visitor_family , 
                                         family %in% FVtoKeep_family)
@@ -50,7 +64,15 @@ floral_visitor_group <-stats::aggregate(number_visits ~ group + year +plot + cod
                                         data = floral_visitor_group,
                                         sum)
 
-floral_visitor_group <- dplyr::filter(floral_visitor_group , group %in% FVtoKeep_group)
+test.fv.group <- floral_visitor_group %>%
+  group_by(group) %>%
+  summarise(mean.sp = mean(number_visits,na.rm = T),
+            var.sp = var(number_visits,na.rm = T)) %>%
+  filter(mean.sp<=var.sp) 
+FVtoKeep_group <- test.fv.group$group[test.fv.group$group %in% FVtoKeep_group]
+
+floral_visitor_group <- dplyr::filter(floral_visitor_group , 
+                                      group %in% FVtoKeep_group)
 
 
 floral_visitor_group  <-  spread(  floral_visitor_group,group, number_visits)
@@ -62,12 +84,11 @@ length(which(!names( floral_visitor_group) %in% c("plot","year","subplot","code.
 
 #---- herbivor ----
 HtoKeep_species <- groupinglevels$grouping[which(groupinglevels$HTL =="herbivore" &
-                                                   groupinglevels$complexity == "species" & groupinglevels$YesNo == "yes")]
+                                                   groupinglevels$complexity == "species" & (groupinglevels$YesNo == "maybe"|groupinglevels$YesNo == "yes"))]
 HtoKeep_family <- groupinglevels$grouping[which(groupinglevels$HTL =="herbivore" &
-                                                  groupinglevels$complexity == "family" & groupinglevels$YesNo == "yes")]
+                                                  groupinglevels$complexity == "family" & (groupinglevels$YesNo == "maybe"|groupinglevels$YesNo == "yes"))]
 HtoKeep_group <- groupinglevels$grouping[which(groupinglevels$HTL =="herbivore" &
-                                                 groupinglevels$complexity == "functional.groups" & groupinglevels$YesNo == "yes")]
-
+                                                 groupinglevels$complexity == "functional.groups" & (groupinglevels$YesNo == "maybe"|groupinglevels$YesNo == "yes"))]
 
 
 levels(as.factor(herbivorypredator$type))
@@ -80,6 +101,14 @@ herbivore_species <- dplyr::select( herbivore, c("day","month","year","plot","su
 herbivore_species <-stats::aggregate(number_animal ~ id_final + year +plot + code.plant +subplot ,
                                      data=  herbivore_species,
                                      sum)
+test.h.sp <- herbivore_species %>%
+  group_by(id_final) %>%
+  summarise(mean.sp = mean(number_animal),
+            var.sp = var(number_animal)) %>%
+  filter(mean.sp<=var.sp) 
+
+HtoKeep_species <- test.h.sp$id_final[test.h.sp$id_final %in% HtoKeep_species]
+
 herbivore_species <- dplyr::filter(herbivore_species, 
                                    id_final %in% HtoKeep_species)
 
@@ -97,6 +126,13 @@ herbivore_family <- dplyr::select( herbivore,c("day","month","year","plot","subp
 herbivore_family <-stats::aggregate(number_animal ~ family + year +plot  + code.plant +subplot ,
                                     data=   herbivore_family,
                                     sum)
+test.h.fam <- herbivore_family %>%
+  group_by(family) %>%
+  summarise(mean.sp = mean(number_animal),
+            var.sp = var(number_animal)) %>%
+  filter(mean.sp<=var.sp) 
+
+HtoKeep_family <- test.h.fam$family[test.h.fam$family %in% HtoKeep_family]
 
 herbivore_family <- dplyr::filter(herbivore_family, family %in% HtoKeep_family)
 herbivore_family  <-  spread(   herbivore_family,family, number_animal)
@@ -111,6 +147,14 @@ herbivore_group <- dplyr::select( herbivore,c("day","month","year","plot","subpl
 herbivore_group <-stats::aggregate(number_animal ~ group + year + plot  + code.plant +subplot ,
                                    data=   herbivore_group,
                                    sum)
+test.h.group <- herbivore_group %>%
+  group_by(group) %>%
+  summarise(mean.sp = mean(number_animal),
+            var.sp = var(number_animal)) %>%
+  filter(mean.sp<=var.sp) 
+
+HtoKeep_group <- test.h.group$group[test.h.group$group %in% HtoKeep_group]
+
 herbivore_group <- dplyr::filter(herbivore_group, group %in% HtoKeep_group)
 
 herbivore_group  <-  spread(   herbivore_group,group, number_animal)
@@ -121,3 +165,26 @@ head(herbivore_group)
 length(which(!names(herbivore_group) %in% c("plot","year","subplot","code.plant")))
 
 
+H.obs.variation <- bind_rows(test.h.sp %>%
+                               mutate(grouping="species") %>%
+                               rename("ID"=id_final),
+                             test.h.fam %>%
+                               mutate(grouping="family") %>%
+                               rename("ID"=family),
+                             test.h.group %>%
+                               mutate(grouping="group") %>%
+                               rename("ID"=group))
+write.csv(H.obs.variation ,
+          file=paste0(home.dic,"results/H.obs.variation.csv"))
+
+FV.obs.variation <- bind_rows(test.fv.sp %>%
+                                mutate(grouping="species") %>%
+                                rename("ID"=id_final),
+                              test.fv.fam %>%
+                                mutate(grouping="family") %>%
+                                rename("ID"=family),
+                              test.fv.group %>%
+                                mutate(grouping="group") %>%
+                                rename("ID"=group))
+write.csv(FV.obs.variation ,
+          file=paste0(home.dic,"results/FV.obs.variation.csv"))
